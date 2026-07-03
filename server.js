b63ac92db10273251227a5abb21bf96faac534e9
yashaswi <yashaswi.iskala@gmail.com>
2026-07-03 13:34:51 +0530

    added frontend

---
+++ server.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const app=express()
+const PORT=3002;
+
+//web server:
+app.use(express)
+app.listen(PORT,function(){
+    console.log("successfully running at http://localhost:"+PORT);
+
+})
