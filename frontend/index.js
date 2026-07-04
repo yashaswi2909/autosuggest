de0d264e22fce131e66420b1b0b712ca46157a37
yashaswi <yashaswi.iskala@gmail.com>
2026-07-04 10:08:07 +0530

    fix commit

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,55 @@
-console.log("hello from js");
-alert("hello from js");
+const users = [
+    {
+        name: "Jane Doe",
+        gender: "Female",
+        image: "images/jane.png"
+    },
+    {
+        name: "John Doe",
+        gender: "Male",
+        image: "images/john.png"
+    }
+];
+
+let curUserId = 0;
+
+function toggle() {
+
+    if(curUserId === 0){
+        curUserId = 1;
+    }
+    else{
+        curUserId = 0;
+    }
+
+    document.getElementById("img").src = users[curUserId].image;
+
+    document.getElementById("card-name").innerText = users[curUserId].name;
+
+    document.getElementById("card-gender").innerText = users[curUserId].gender;
+    
+}
+function getRandomUser()
+{
+  fetch("https://randomuser.me/api")
+    .then(function (data){
+      return data.json()
+    })
+    .then(function (parsedData){
+      // console.log(parsedData)
+      let gender = parsedData.results[0].gender;
+
+      let first = parsedData.results[0].name.first;
+      let last = parsedData.results[0].name.last;
+      let name = first  + " " + last;
+      let imageUrl = parsedData.results[0].picture.large;
+
+      // gender
+      document.getElementById("card-gender").innerText=gender;
+      // name
+      document.getElementById("card-name").innerText=name
+      // img
+      document.getElementById("img").src=imageUrl
+
+    })
+}
